2d8698c7225ba0afc1347f75f65de8976e865d43
Irving Zhang <702139104@qq.com>
2024-01-30 23:53:52 +0800

    add comments

--- game/snake.go
+++ game/snake.go
@@ -21,12 +21,12 @@ const (
 )
 
 type snake struct {
-	locations *list.List
+	units     *list.List
 	direction int
 }
 
 func (s *snake) getHead() *pos {
-	p, ok := s.locations.Front().Value.(pos)
+	p, ok := s.units.Front().Value.(pos)
 	if ok {
 		return &p
 	}
@@ -34,13 +34,14 @@ func (s *snake) getHead() *pos {
 }
 
 func (s *snake) getTail() *pos {
-	p, ok := s.locations.Back().Value.(pos)
+	p, ok := s.units.Back().Value.(pos)
 	if ok {
 		return &p
 	}
 	return nil
 }
 
+// updateLocation update snake location
 func (s *snake) updateLocation() (bool, interface{}) {
 	headPos := s.getHead()
 	var frontPos pos
@@ -59,16 +60,18 @@ func (s *snake) updateLocation() (bool, interface{}) {
 	if !s.validateBody(&frontPos) {
 		return false, nil
 	}
-	s.locations.PushFront(frontPos)
-	return true, s.locations.Remove(s.locations.Back())
+	s.units.PushFront(frontPos)
+	return true, s.units.Remove(s.units.Back())
 }
 
+// growUp make snake longer by one unit at tail
 func (s *snake) growUp(p pos) {
-	s.locations.PushBack(p)
+	s.units.PushBack(p)
 }
 
+// validateBody check whether the snake head hit the body units
 func (s *snake) validateBody(p *pos) bool {
-	for loc := s.locations.Front(); loc != nil; loc = loc.Next() {
+	for loc := s.units.Front(); loc != nil; loc = loc.Next() {
 		bodyPos := loc.Value.(pos)
 		if reflect.DeepEqual(p, &bodyPos) {
 			return false
@@ -82,6 +85,7 @@ func (s *snake) changeDirection(direction int) {
 }
 
 func initSnake(length, height int) *snake {
+	// init the snake at the center part of game map
 	x, y := rand.Intn(length/2)+length/4, rand.Intn(height/2)+height/4
 	location := list.New()
 	location.PushFront(pos{x, y})
